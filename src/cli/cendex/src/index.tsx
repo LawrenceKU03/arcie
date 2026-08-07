415416fea1826fbf0df0db031ccb97ae5fecf9d9
Lawrence <lawuche29@gmail.com>
2026-08-07 13:30:57 +0100

    chore:did tui setup

--- src/cli/cendex/src/index.tsx
+++ src/cli/cendex/src/index.tsx
@@ -2,14 +2,19 @@ import { createCliRenderer, TextAttributes } from "@opentui/core";
 import { createRoot } from "@opentui/react";
 
 function App() {
-  return (
-    <box alignItems="center" justifyContent="center" flexGrow={1}>
-      <box justifyContent="center" alignItems="flex-end">
-        <ascii-font font="tiny" text="OpenTUI" />
-        <text attributes={TextAttributes.DIM}>What will you build?</text>
-      </box>
-    </box>
-  );
+	return (
+		<box alignItems="center" justifyContent="center" flexGrow={1}>
+			<box
+				justifyContent="center"
+				alignItems="center"
+				flexDirection="row"
+				gap={2}
+			>
+				<ascii-font font="slick" text="CENCORI" color="gray" />
+				<ascii-font font="slick" text="CODE" />
+			</box>
+		</box>
+	);
 }
 
 const renderer = await createCliRenderer();
